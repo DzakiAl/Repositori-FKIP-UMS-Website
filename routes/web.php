668e5513cbb9572a9e6e76f8957386c3caf42254
Dzaki Al <113689655+DzakiAl@users.noreply.github.com>
2025-02-20 13:58:16 +0700

    make the code more neat

--- routes/web.php
+++ routes/web.php
@@ -8,17 +8,17 @@ use App\Http\Controllers\UserController;
 Route::get('/', [RepositoryController::class, 'index'])->name('repository.index');
 Route::get('/file-manager/{type}/{program}/{subfolder?}', [RepositoryController::class, 'file_manager'])->where('subfolder', '.*')->name('repository.file_manager');
 Route::post('/upload-file/{type}/{program}/{subfolder?}', [RepositoryController::class, 'upload_file'])->where('subfolder', '.*')->name('repository.upload_file');
+Route::post('/upload-folder/{type}/{program}/{subfolder?}', [RepositoryController::class, 'upload_folder'])->where('subfolder', '.*')->name('repository.upload_folder');
 Route::post('/add-folder/{type}/{program}/{subfolder?}', [RepositoryController::class, 'add_folder'])->where('subfolder', '.*')->name('repository.add_folder');
 Route::get('/delete-folder/{type}/{program}/{subfolder?}/{folder}', [RepositoryController::class, 'delete_folder'])->where('subfolder', '.*')->name('repository.delete_folder');
 Route::get('/download-file/{type}/{program}/{subfolder?}/{file}', [RepositoryController::class, 'download_file'])->where('subfolder', '.*')->name('repository.download_file');
 Route::post('/download-folder/{type}/{program}/{subfolder?}', [RepositoryController::class, 'download_folder'])->where('subfolder', '.*')->name('repository.download_folder');
 Route::get('/delete-file/{type}/{program}/{subfolder?}/{file}', [RepositoryController::class, 'delete_file'])->where('subfolder', '.*')->name('repository.delete_file');
 Route::get('/open-file/{type}/{program}/{subfolder?}/{file}', [RepositoryController::class, 'open_file'])->where('subfolder', '.*')->name('repository.open_file');
+Route::post('/rename/{type}/{program}/{subfolder?}', [RepositoryController::class, 'rename'])->where('subfolder', '.*')->name('repository.rename');
 Route::get('/login', [AuthController::class, 'ShowLoginForm'])->name('show_login_form');
 Route::post('/login', [AuthController::class, 'Login'])->name('login');
 Route::post('/logout', [AuthController::class, 'Logout'])->name('logout');
-Route::post('/rename/{type}/{program}/{subfolder?}', [RepositoryController::class, 'rename'])->where('subfolder', '.*')->name('repository.rename');
-Route::post('/upload-folder/{type}/{program}/{subfolder?}', [RepositoryController::class, 'upload_folder'])->where('subfolder', '.*')->name('repository.upload_folder');
 
 Route::middleware(['auth'])->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'dashboard'])->name('dashboard');
